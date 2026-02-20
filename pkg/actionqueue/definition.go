038c8ae8c7255ac9992fe34c5514b59466650ae7
Justin Gallardo <justin.gallardo@conductorone.com>
2026-02-20 11:52:03 +0000

    Update definition for improved constructor wiring

--- pkg/actionqueue/definition.go
+++ pkg/actionqueue/definition.go
@@ -5,6 +5,10 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// DriverConstructorFunc creates a DriverFactory from shared dependencies.
+// Queue driver packages register one of these to be called during service startup.
+type DriverConstructorFunc func(deps any) any
+
 // Definition describes an action queue backed by a Temporal workflow.
 type Definition struct {
 	// Type-level fields (set by generated code in init())
@@ -15,6 +19,9 @@ type Definition struct {
 	TenantIDFunc       func(proto.Message) string
 	EntityIDsFunc      func(proto.Message) []string
 	WorkflowIDFromArgs func(tenantID string, entityIDs []string) string
+
+	// Driver constructor (set by queue driver packages via SetDriverConstructor)
+	DriverConstructor DriverConstructorFunc
 
 	// Runtime fields (set when driver is bound)
 	ActivityMain    any
